88be7461029dc67d0215e5541c5ed904647bed85
Neil Chowdhury <neil.chowdhury@outlook.com>
2019-07-24 00:04:34 -0700

    add support for choosing an answer in a multiple choice question

--- src/curricula/components/MultipleChoiceQuestion.js
+++ src/curricula/components/MultipleChoiceQuestion.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native'
+import {ButtonGroup} from 'react-native-elements'
 
 class MultipleChoiceQuestion extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            selectedChoice: null
+        }
+    }
+
+    updateChoice(selectedIndex) {
+        console.log(selectedIndex)
+        this.setState({selectedChoice: selectedIndex})
     }
 
     render() {
+        const buttons = this.props.question.choices.map((choice) => {
+            return ({element: () => <View key={choice.uuid}>
+                <Text>{choice.content.text}</Text>
+                {choice.content.image? <Image 
+                style={{width: 100, height: 100, resizeMode: 'contain'}}
+                 source={{uri: choice.content.image}}></Image>:null}
+            </View>})
+        })
+
+        // console.log(this.props)
         return (
             <View>
                 <Text>{this.props.question.text}</Text>
-                {this.props.question.choices.map((choice) => {
-                    return <View key={choice.uuid}>
-                        {/* <Text>{choice.content.text}</Text> */}
-                        <Image 
-                        // style={{width: 100, height: 100, resizeMode: 'contain'}}
-                         source={{uri: choice.content.image}}></Image>
-                    </View>
-                })}
+                {this.props.question.image? <Image 
+                    style={{width: 100, height: 100, resizeMode: 'contain'}}
+                    source={{uri: this.props.question.image}}></Image>:null}
+                <ButtonGroup
+                    onPress={this.updateChoice.bind(this)}
+                    buttons={buttons}
+                    selectedIndex={this.state.selectedChoice}
+                ></ButtonGroup>
             </View>
         )
     }
