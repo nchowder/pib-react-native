69e14902573eeee5a1cfe386b537ab33710e0ecd
nchowder <neil.chowdhury@outlook.com>
2020-01-05 16:04:11 -0800

    fix auth reducer name

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux'
 import curriculumReducer from '../curricula/reducers'
-import authReducer from '../curricula/reducers'
+import authReducer from '../auth/reducers'
 
 export default combineReducers({
   curriculum: curriculumReducer,
